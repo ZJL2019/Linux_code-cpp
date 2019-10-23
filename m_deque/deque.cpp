b4dd18b6c2942d18beec413928458b9f799af0b0
ZJL2019 <379105244@qq.com>
2019-10-23 14:07:32 +0800

    添加使用双端队列实现队列容器

--- m_deque/deque.cpp
+++ m_deque/deque.cpp
@@ -43,6 +43,62 @@ using namespace std;
       Container _con;
   };
 
+
+template<class T,class Container=deque<T> >
+class Queue
+{
+  public:
+    Queue()
+    {}
+
+    void push(const T& data)
+    {
+      _con.push_back(data);
+    }
+
+    void pop()
+    {
+      _con.pop_front();
+    }
+
+    T& front()
+    {
+      return _con.front();
+    }
+
+    const T& front()const
+    {
+      return _con.front();
+    }
+
+    T& back()
+    {
+      return _con.back();
+    }
+
+    const T& back()const 
+    {
+      return _con.back();
+    }
+
+    size_t size()const 
+    {
+      return _con.size();
+    }
+
+    bool empty()const 
+    {
+      return _con.empty();
+    }
+
+  private:
+    Container _con;
+
+};
+
+
+
+
 #include <vector>
 void TestStack()
 {
@@ -63,10 +119,47 @@ void TestStack()
   cout<<s2.top()<<endl;
 }
 
+#include <list>
+
+void Testqueue()
+{
+  Queue<int> q1;
+  q1.push(1);
+  q1.push(2);
+  q1.push(3);
+  q1.push(4);
+  cout<<q1.size()<<endl;
+  cout<<q1.front()<<endl;
+  cout<<q1.back()<<endl;
+
+  q1.pop();
+  q1.pop();
+  cout<<q1.size()<<endl;
+  cout<<q1.front()<<endl;
+  cout<<q1.back()<<endl;
+
+  Queue<int,list<int> > q2;
+  q2.push(1);
+  q2.push(2);
+  q2.push(3);
+  q2.push(4);
+  cout<<q2.size()<<endl;
+  cout<<q2.front()<<endl;
+  cout<<q2.back()<<endl;
+
+  q2.pop();
+  q2.pop();
+  cout<<q2.size()<<endl;
+  cout<<q2.front()<<endl;
+  cout<<q2.back()<<endl;
+
+}
+
 int main()
 {
  
-TestStack();
+  //TestStack();
+  Testqueue();
   return 0;
 }
 
