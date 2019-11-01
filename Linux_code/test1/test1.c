06059dc0b0cc7aad410a44b3876be58cb15b918a
ZJL2019 <379105244@qq.com>
2019-11-01 12:46:14 +0800

    优化进度颜色

--- Linux_code/test1/test1.c
+++ Linux_code/test1/test1.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 int main()
 {
-  char buf[1024]={0};
+  char buf[100]={0};
   const char* label="/-\\|";
   int i=0;
   int j=0;
@@ -12,15 +12,15 @@ int main()
     buf[i]='#';
     if(i<6)
     { 
-    printf("\033[0;31m%d%%[%c][%s]\r\033[0;39m",j,label[i%4],buf);
+    printf("\033[0;31m%d%%\033[0;39m[%c][%s]\r",j,label[i%4],buf);
     }
     else if(i>=6&&i<12)
     {  
-    printf("\033[0;33m%d%%[%c][%s]\r\033[0;39m",j,label[i%4],buf);
+    printf("\033[0;33m%d%%\033[0;39m[%c][%s]\r",j,label[i%4],buf);
     }
     else
     {
-    printf("\033[0;32m%d%%[%c][%s]\r\033[0;39m",j,label[i%4],buf);
+    printf("\033[0;32m%d%%\033[0;39m[%c][%s]\r",j,label[i%4],buf);
     }
     fflush(stdout);
     j= j+5;
