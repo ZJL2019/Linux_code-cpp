a24597b4eb9115521d3c31de59b623b13f5c56c5
ZJL2019 <379105244@qq.com>
2019-11-04 14:02:06 +0800

    添加

--- C++_code/inherit/inherit.cpp
+++ C++_code/inherit/inherit.cpp
@@ -1201,6 +1201,31 @@ class B
   public:
     int _b;
 };
+class C1:virtual public B
+{
+  public:
+    int _c1;
+};
+class C2:virtual public B
+{
+  public:
+    int _c2;
+};
+class D: public C1,public C2
+{
+  public:
+    int _d;
+};
+
+int main()
+{
+  D d;
+  d._b=1;
+  d._c1=2;
+  d._c2=3;
+  d._d=4;
+  return 0;
+}
 
 
 
