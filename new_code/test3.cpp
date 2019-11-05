ecca526b5eed5f4b1277a8be893b05dcd9c4296c
ZJL2019 <379105244@qq.com>
2019-11-05 12:42:55 +0800

    剑指offer-3

---
+++ new_code/test3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    bool duplicate(int numbers[], int length, int* duplication) {
+        if(numbers==NULL||length<0)
+        {
+            return false;
+        }
+        for(int i=0;i<length;i++)
+        {
+            while(numbers[i]!=i)
+            {
+                if(numbers[i]==numbers[numbers[i]])
+                {
+                    duplication[0]=numbers[i];
+                    return true;
+                }
+                swap(numbers,i,numbers[i]);
+            }
+        }
+        return false;
+    }
+private:
+    void swap(int number[],int i,int j)
+    {
+        int t=number[i];
+        number[i]=number[j];
+        number[j]=t;
+    }
+};
