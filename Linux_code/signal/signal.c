dcba463a0798b5e6d81f491e081da428da8ac731
ZJL2019 <379105244@qq.com>
2020-01-02 17:43:07 +0800

    信号捕捉

---
+++ Linux_code/signal/signal.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+
+int g_val=0;
+
+void sigcallback(int sig)
+{
+    g_val++;
+    printf("sig:%d\n",sig);
+}
+
+int main()
+{
+    signal(2,sigcallback);
+    
+    int count=100;
+    while(count--)
+    {
+        g_val++;
+        usleep(100000);
+    }
+    printf("g_val:%d\n",g_val);
+    return 0;
+}
