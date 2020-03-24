69cbb67487a4a60c2baf9191398a291339201852
ZJL2019 <379105244@qq.com>
2020-03-24 21:16:35 +0800

    完善html渲染调用，以及点击跳转

--- Linux_code/online_oj/src/oj_server.cpp
+++ Linux_code/online_oj/src/oj_server.cpp
@@ -1,6 +1,8 @@
 #include "httplib.h"
 
 #include "oj_model.hpp"
+#include "oj_view.hpp"
+#include "oj_log.hpp"
 
 int main()
 {
@@ -8,9 +10,24 @@ int main()
     Server svr;
     OjModel ojmodel;
     svr.Get("/all_questions",[&ojmodel](const Request& req,Response& resp){
+            (void)req;
+
             std::vector<Topic> topic; //通过出参获取题目信息
             ojmodel.GetAllTpoic(&topic);
-            resp.set_content("","text/html");
+
+            std::string html;
+            OJ_View::ExpandAllQuestionshtml(&html,topic);
+            LOG(INFO,html)
+            resp.set_content(html,"text/html;charset=UTF-8");
+            });
+
+    svr.Get(R"(/question/\d)",[&ojmodel](const Request& req,Response& resp){
+            (void)ojmodel;
+            (void)req;
+
+            std::string html="1";
+            LOG(INFO,html);
+            resp.set_content(html,"text/html;charset=UTF-8");
             });
     svr.listen("0.0.0.0",9090);
     return 0;
