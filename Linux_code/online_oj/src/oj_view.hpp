52b4302f27fa13cf3f4c0c1468fdfe8afeb48f7b
ZJL2019 <379105244@qq.com>
2020-03-28 16:52:40 +0800

    添加渲染单个问题界面的html模板方法

--- Linux_code/online_oj/src/oj_view.hpp
+++ Linux_code/online_oj/src/oj_view.hpp
@@ -19,5 +19,16 @@ class OJ_View
             ctemplate::Template* tl=ctemplate::Template::GetTemplate("./html_template/all_questions.html",ctemplate::DO_NOT_STRIP);
             tl->Expand(html,&dict);
         }
-
+        
+        static void ExpandOneQuestion(std::string* html,const Topic& topic,std::string& desc,std::string& header)
+        {
+            ctemplate::TemplateDictionary dict("question");
+            dict.SetValue("id",topic.id_);
+            dict.SetValue("name",topic.name_);
+            dict.SetValue("star",topic.star_);
+            dict.SetValue("desc",desc);
+            dict.SetValue("header",header);
+            ctemplate::Template* tpl=ctemplate::Template::GetTemplate("./html_template/question.html",ctemplate::DO_NOT_STRIP);
+            tpl->Expand(html,&dict);
+        }
 };
