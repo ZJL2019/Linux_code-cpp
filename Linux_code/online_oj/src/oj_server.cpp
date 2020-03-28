21132dac3b4b54740cf79c8c5b627e04aaf5ad19
ZJL2019 <379105244@qq.com>
2020-03-28 16:50:52 +0800

    添加渲染单个问题的界面

--- Linux_code/online_oj/src/oj_server.cpp
+++ Linux_code/online_oj/src/oj_server.cpp
@@ -21,14 +21,25 @@ int main()
             resp.set_content(html,"text/html;charset=UTF-8");
             });
 
-    svr.Get(R"(/question/\d)",[&ojmodel](const Request& req,Response& resp){
-            (void)ojmodel;
-            (void)req;
+    svr.Get(R"(/question/(\d+))",[&ojmodel](const Request& req,Response& resp){
 
-            std::string html="1";
+            std::string desc;
+            std::string header;
+            std::string tmp;
+            tmp+="req.mathches"+req.matches[0].str()+":"+req.matches[1].str();
+            LOG(INFO,tmp)
+            
+            struct Topic topic; 
+            ojmodel.GetOneTopic(req.matches[1].str(),&desc,&header,&topic);
+
+            std::string html;
+            OJ_View::ExpandOneQuestion(&html,topic,desc,header);
             LOG(INFO,html);
+            
             resp.set_content(html,"text/html;charset=UTF-8");
             });
+    LOG(INFO,"listen in 0.0.0.0:9090");
+    LOG(INFO,"Server ready");
     svr.listen("0.0.0.0",9090);
     return 0;
 }
