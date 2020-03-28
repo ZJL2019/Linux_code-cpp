951e57ab581c4977f6abefa9565d548c4a1f6549
ZJL2019 <379105244@qq.com>
2020-03-28 16:51:40 +0800

    读取单个题目文件信息

--- Linux_code/online_oj/src/oj_tools.hpp
+++ Linux_code/online_oj/src/oj_tools.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 #include <boost/algorithm/string.hpp>
-
+#include <fstream>
+#include "oj_log.hpp"
 #define CHECK_RET(exp) if(!(exp)) {return false;}
 
 
@@ -13,5 +15,30 @@ class StringTools
         static void Split(std::string& input,const std::string& split_char,std::vector<std::string>* output)
         {
             boost::split(*output,input,boost::is_any_of(split_char),boost::token_compress_off);
+        }
+};
+
+
+class FileOper
+{
+    public:
+        static int ReadDateFromFile(const std::string& filename,std::string* content)
+        {
+            std::ifstream file(filename.c_str());
+            if(!file.is_open())
+            {
+                std::string tmp;
+                tmp+="Open file failed filename is "+filename;
+                LOG(ERROR,tmp);
+                return -1;
+            }
+            
+            std::string line;
+            while(std::getline(file,line))
+            {
+                *content+=line+"\n";
+            }
+            file.close();
+            return 0;
         }
 };
